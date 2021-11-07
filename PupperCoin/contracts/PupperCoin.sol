59013162aa9b25d7ab61191768746aaca9f0547f
MrSquiggles13 <ayresguitar@gmail.com>
2021-11-07 11:24:53 -0500

    Migrated Homework Folder

---
+++ PupperCoin/contracts/PupperCoin.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "OpenZeppelin/openzeppelin-contracts@2.5.0/contracts/token/ERC20/ERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "OpenZeppelin/openzeppelin-contracts@2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract PupperCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint initial_supply
+    )
+        ERC20Detailed(name, symbol, 18)
+        public
+    {
+        // constructor can stay empty
+    }
+}
